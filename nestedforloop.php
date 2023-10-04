2958e589b87374928cbe3bee971ebb428d24a5fd
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2023-10-04 10:54:56 +0100

    added table

---
+++ nestedforloop.php
@@ -0,0 +1,14 @@
+<?php
+#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
+#put your name and the date here
+#put a descripition of what the code does here - 
+#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
+
+for ($i = 0; $i <= 100; $i++) {
+    echo "$i<2; $i";
+    
+    if ($i % 10 == 0) { 
+        echo "<br>";   
+    }
+}
+?>
